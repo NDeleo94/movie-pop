7a3cbb82a4c67dec2c5a284fb18ad374c03f4853
NDeleo94 <n.deleo94@gmail.com>
2022-07-01 11:56:13 -0300

    Se trabajo en los estilos de GridPage

--- src/pages/GridPage.jsx
+++ src/pages/GridPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MoviesGrid from "../components/MoviesGrid";
 import Search from "../components/Search";
+import styles from "../pages styles/GridPage.module.css";
 
 const GridPage = () => {
   return (
     <div>
       <Search />
-      <h2>Películas</h2>
-      <hr />
+      <h1 className={styles.h1}>Películas</h1>
+      <hr className={styles.hr} />
       <MoviesGrid />
     </div>
   );
