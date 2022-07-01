faabd3deab1634435bb12fc00beaed873017d3fd
NDeleo94 <n.deleo94@gmail.com>
2022-07-01 01:06:35 -0300

    Se trabajo en los estilos de NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,44 @@
+import {
+  faArrowLeft,
+  faHouseChimney,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "../components styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.navList}>
         <li>
-          <NavLink to={-1}>back</NavLink>
+          <NavLink to={-1} className={styles.colorA}>
+            {" "}
+            <FontAwesomeIcon icon={faArrowLeft} size={"2x"} />
+          </NavLink>
         </li>
         <li>
-          <NavLink to={"/buscar"}>Buscar</NavLink>
+          <NavLink
+            to={"/buscar"}
+            className={({ isActive }) =>
+              isActive ? styles.colorB : styles.colorA
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faSearch} size={"2x"} />
+          </NavLink>
         </li>
         <li>
-          <NavLink to={"/"}>Home</NavLink>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive ? styles.colorB : styles.colorA
+            }
+          >
+            {" "}
+            <FontAwesomeIcon icon={faHouseChimney} size={"2x"} />
+          </NavLink>
         </li>
       </ul>
     </nav>
