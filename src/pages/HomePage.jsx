78a5e6dc3f112ce3b706429121c16300ecee83eb
NDeleo94 <n.deleo94@gmail.com>
2022-06-30 23:35:05 -0300

    Se trabajo en los estilos de HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from "../images/cinema.png";
+import styles from  "../pages styles/HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>MoviePop!</p>
+    <header className={styles.header}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <p className={styles.p}>MoviePop!</p>
     </header>
   )
 }
