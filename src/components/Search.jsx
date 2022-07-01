b2e8b6f58e178f2446384c4072b3560d3e756a58
NDeleo94 <n.deleo94@gmail.com>
2022-07-01 12:04:32 -0300

    Se trabajo en los estilos de Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "../components styles/Search.module.css";
 
 const Search = () => {
   return (
-    <form>
-      <div>
-        <input />
+    <form className={styles.searchContainer}>
+      <div className={styles.searchBox}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          placeholder="Buscar"
+        />
       </div>
     </form>
   );
