52dd9b00fe50922f19733eedcb41478e3e10b20e
NDeleo94 <n.deleo94@gmail.com>
2022-07-01 11:26:20 -0300

    Se trabajo en la estructura de MoviesGrid

--- src/components/MovieGridItem.jsx
+++ src/components/MovieGridItem.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import image from "../images/unavailable.jpeg";
+import movies from "../data.json";
 
 const MovieGridItem = () => {
   return (
     <li>
-      <Link to="/pelicula">
-        <img src={image} alt="movie poster"/>
-        <div></div>
+      <Link to={"/pelicula/" + movies[0].show.id}>
+        <img
+          width={230}
+          height={345}
+          src={movies[0].show.image.original}
+          alt={"poster" + movies[0].show.name}
+        />
+        <div>{movies[0].show.name}</div>
       </Link>
     </li>
   );
