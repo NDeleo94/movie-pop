efa34326fa3c7583af0e669da7ca776d49efc941
NDeleo94 <n.deleo94@gmail.com>
2022-07-04 13:50:37 -0300

    Se agrega boton favoritos a NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,20 +3,28 @@ import {
   faHouseChimney,
   faSearch,
   faUser,
+  faHeart,
+  faArrowRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { NavLink, useMatch } from "react-router-dom";
 import styles from "../components styles/NavBar.module.css";
+import { useSelector } from "react-redux/es/exports";
 
 const NavBar = () => {
   const match = useMatch("/pelicula/:id");
+  const { auth } = useSelector((state) => state);
 
   const estilos = (active) => {
     let estilo = ``;
     active ? (estilo += `${styles.colorB} `) : (estilo += `${styles.colorA} `);
     match ? (estilo += `${styles.hidden} `) : (estilo += ``);
     return estilo;
+  };
+
+  const isAuth = () => {
+    return !auth.uid ? styles.hidden : "";
   };
 
   return (
@@ -33,17 +41,32 @@ const NavBar = () => {
         </li>
         <li>
           <NavLink
+            to={"/login"}
+            className={({ isActive }) => estilos(isActive)}
+          >
+            {" "}
+            <FontAwesomeIcon
+              icon={auth.uid ? faArrowRightFromBracket : faUser}
+              size={"2x"}
+            />
+          </NavLink>
+        </li>
+        <li className={isAuth()}>
+          <NavLink
+            to={"/favoritos"}
+            className={({ isActive }) => estilos(isActive)}
+          >
+            {" "}
+            <FontAwesomeIcon icon={faHeart} size={"2x"} />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
             to={"/buscar"}
             className={({ isActive }) => estilos(isActive)}
           >
             {" "}
             <FontAwesomeIcon icon={faSearch} size={"2x"} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/login"} className={({ isActive }) => estilos(isActive)}>
-            {" "}
-            <FontAwesomeIcon icon={faUser} size={"2x"} />
           </NavLink>
         </li>
         <li>
