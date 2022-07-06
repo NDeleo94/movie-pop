78d108e97b838c15ba56cf72d1d6a5042b2c8529
NDeleo94 <n.deleo94@gmail.com>
2022-07-06 10:31:51 -0300

    Cambio implementacino de consulta

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -1,17 +1,18 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 export const loadData = async (uid) => {
-    const response = await getDocs(collection(db, `${uid}/fav/favoritas`));
-    const data = [];
-  
-    response.forEach((fav) => {
-      const favData = fav.data();
-      data.push({
-        id: fav.id,
-        ...favData,
-      });
+  const first = query(collection(db, `${uid}/fav/favoritas`));
+  const response = await getDocs(first);
+
+  const data = [];
+
+  response.forEach((fav) => {
+    const favData = fav.data();
+    data.push({
+      id: fav.id,
+      ...favData,
     });
-    return data;
-  };
-  
+  });
+  return data;
+};
