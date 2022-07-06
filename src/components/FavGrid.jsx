ec8b3dc78d87ef76810370aacaa3b7e9f9da2d2e
NDeleo94 <n.deleo94@gmail.com>
2022-07-06 12:02:56 -0300

    Se implemento Infinite Scroll en FavPage

--- src/components/FavGrid.jsx
+++ src/components/FavGrid.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux/es/exports";
 import { getFav } from "../utils/getFav";
 import NoResults from "./NoResults";
 import styles from "../components styles/FavGrid.module.css";
 import MovieGridItem from "./MovieGridItem";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loading from "./Loading";
 
 const FavGrid = () => {
   const { fav } = useSelector((state) => state);
   const listaFav = getFav(fav.favoritas);
 
+  const [peliculas, setPeliculas] = useState([]);
+  const [next, setNext] = useState(0);
+
+  useEffect(() => {
+    if (listaFav.length < 12) {
+      setPeliculas(listaFav);
+    } else {
+      setPeliculas((prevPeli) =>
+        prevPeli.concat(listaFav.slice(next, next + 12))
+      );
+    }
+  }, [next]);
+
   if (listaFav.length === 0) {
     return <NoResults text={"Aun no agregaste favoritas"} />;
   }
+
   return (
-    <ul className={styles.moviesGrid}>
-      {listaFav.map((favorita) => (
-        <MovieGridItem key={favorita.show.id} movie={favorita} />
-      ))}
-    </ul>
+    <InfiniteScroll
+      dataLength={peliculas.length}
+      hasMore={listaFav.length > peliculas.length}
+      next={() => setNext((prevNext) => prevNext + 12)}
+      loader={<Loading />}
+    >
+      <ul className={styles.moviesGrid}>
+        {peliculas.map((favorita) => (
+          <MovieGridItem key={favorita.show.id} movie={favorita} />
+        ))}
+      </ul>
+    </InfiniteScroll>
   );
 };
 
